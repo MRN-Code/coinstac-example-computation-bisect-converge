7281aef5d1b147915f7d71e094b9ac7cec7d12c0
cdaringe <christopher.dieringer@gmail.com>
2016-06-14 15:27:25 -0700

    migrate to latest sim api.

--- test/index.js
+++ test/index.js
@@ -5,5 +5,5 @@ const sim = require('coinstac-simulator');
 const noop = require('lodash/noop');
 
 const declaration = path.resolve(__dirname, './declaration');
-sim.setup(declaration)
+sim.run(declaration)
 .then(() => sim.teardown(noop));
